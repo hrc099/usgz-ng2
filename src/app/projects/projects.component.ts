a395b7108284136ce62e84e835fe4a93422e62f4
hrc099 <hrc099@gmail.com>
2016-12-11 22:58:14 +0100

    Added fuction for toggling text with button click

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,4 +12,25 @@ export class ProjectsComponent implements OnInit {
   ngOnInit() {
   }
 
+  toggleText(text: string, button: string): void {
+    let x = document.getElementById(button);
+    if (x.textContent === 'Pročitaj više ') {
+      x.textContent = '';
+      x.insertAdjacentHTML( 'afterbegin',
+      'Pročitaj manje <i class="fa fa-angle-up" aria-hidden="true"></i>' );
+    }
+    else {
+      x.textContent = '';
+      x.insertAdjacentHTML( 'afterbegin', 
+      'Pročitaj više <i class="fa fa-angle-down" aria-hidden="true"></i>' );
+    }
+    let a = document.getElementById(text);
+    if (a.style.display === "block") {
+      a.style.display = 'none';
+    }
+    else {
+      a.style.display = "block";
+    }
+  }
+
 }
